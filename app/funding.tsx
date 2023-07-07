6df4ff355e98b8debbf76cd7f28be3fc815a1aa4
sohanemon <m.sohanemon@gmail.com>
2023-07-07 14:43:02 +0000

    Jul 7, 2023, 8:43 PM

--- app/funding.tsx
+++ app/funding.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function Funding() {
   return (
-    <section>
+    <section className='container'>
       <div className='text-center'>
         <span className='text-slate-700 text-[25px] font-bold'>
           Direct Funding <br />
@@ -12,12 +12,17 @@ export default function Funding() {
 
       {/* first div */}
       <div className='grid grid-cols-2'>
-        <Image
-          src={`/public/assets/images/transition/money.svg`}
-          alt=''
-          width={50}
-          height={50}
-        />
+        <div>
+          <Image
+            src={`/assets/images/transition/money.svg`}
+            alt=''
+            width={50}
+            height={50}
+          />
+          <p className=' text-slate-700 text-[20px] font-medium'>
+            Channel Funding{' '}
+          </p>
+        </div>
       </div>
     </section>
   );
