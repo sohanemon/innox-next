13d11f25b9bfe6d9b4d8c8aac564f56479ffc55a
sohanemon <m.sohanemon@gmail.com>
2023-07-07 17:54:32 +0000

    Jul 7, 2023, 11:54 PM

--- app/creators.tsx
+++ app/creators.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 export default function Creators() {
   return (
     <section className='flex flex-col w-fit mx-auto my-20'>
@@ -5,53 +7,59 @@ export default function Creators() {
         Trending Creators
       </div>
       <div className='h-1 w-full bg-gradient-to-r from-primary via-primary/70 to-transparent rounded-full' />
-      <div className='flex'></div>
+      <div className='flex'>
+        {data.map((_) => (
+          <div key={_.title}>
+            <Image src={_.value} alt='' width={100} height={100} />
+          </div>
+        ))}
+      </div>
     </section>
   );
 }
 const data = [
   {
-    photo: '/public/assets/images/avt/avt1.svg',
+    photo: '/assets/images/avt/avt1.svg',
     title: 'Crispin Berry',
     value: '214.2 ETH',
   },
   {
-    photo: '/public/assets/images/avt/avt2.svg',
+    photo: '/assets/images/avt/avt2.svg',
     title: 'Samson Frost',
     value: '205.43 ETH',
   },
   {
-    photo: '/public/assets/images/avt/avt3.svg',
+    photo: '/assets/images/avt/avt3.svg',
     title: 'Tommy Alvarez',
     value: '170.3 ETH',
   },
   {
-    photo: '/public/assets/images/avt/avt4.svg',
+    photo: '/assets/images/avt/avt4.svg',
     title: 'Windsor Lane',
     value: '120.7 ETH',
   },
   {
-    photo: '/public/assets/images/avt/avt5.svg',
+    photo: '/assets/images/avt/avt5.svg',
     title: 'Andy Hurlbutt',
     value: '82.79 ETH',
   },
   {
-    photo: '/public/assets/images/avt/avt6.svg',
+    photo: '/assets/images/avt/avt6.svg',
     title: 'Blake Banks',
     value: '68.2 ETH',
   },
   {
-    photo: '/public/assets/images/avt/avt7.svg',
+    photo: '/assets/images/avt/avt7.svg',
     title: 'Monica Lucas',
     value: '52.8 ETH',
   },
   {
-    photo: '/public/assets/images/avt/avt1.svg',
+    photo: '/assets/images/avt/avt1.svg',
     title: 'Matt Ramos',
     value: '38.4 ETH',
   },
   {
-    photo: '/public/assets/images/avt/avt2.svg',
+    photo: '/assets/images/avt/avt2.svg',
     title: 'Harper Wilcher',
     value: '29.2 ETH',
   },
