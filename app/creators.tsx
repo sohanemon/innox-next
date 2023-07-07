56527f14abf6177eb9813ce614617e71071c7a2a
sohanemon <m.sohanemon@gmail.com>
2023-07-07 15:20:17 +0000

    Jul 7, 2023, 9:20 PM

--- app/creators.tsx
+++ app/creators.tsx
@@ -1,9 +1,10 @@
 export default function Creators() {
   return (
-    <section>
+    <section className='flex flex-col w-fit mx-auto my-20'>
       <div className='text-center text-sky-950 text-[25px] font-bold capitalize leading-10'>
         Trending Creators
       </div>
+      <div className='h-1 w-full bg-gradient-to-r from-primary to-transparent rounded-full' />
     </section>
   );
 }
