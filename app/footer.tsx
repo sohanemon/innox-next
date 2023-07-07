4e676b764c465ae8101426e13d0ddda24810ef14
sohanemon <m.sohanemon@gmail.com>
2023-07-07 19:01:15 +0000

    Jul 8, 2023, 1:01 AM

--- app/footer.tsx
+++ app/footer.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 
 export default function Footer() {
   return (
-    <section className='flex flex-wrap justify-between gap-10 py-10 mt-32 bg-white'>
+    <section className='flex flex-wrap justify-between gap-10 py-10 px-3 mt-32 bg-[#f8f8f8]'>
       <div className='space-y-4'>
         <Brand />
         <p className='text-neutral-800 text-[14px] font-normal leading-snug'>
@@ -54,9 +54,28 @@ export default function Footer() {
         >
           Signup for our newsletter to get the latest news in your inbox.
         </Link>
-        <div>
-          <input type='text' placeholder='Info@yourgmail.com' />
-          <button></button>
+        <div className='flex items-stretch overflow-hidden border rounded-lg border-black/5 w-fit '>
+          <input
+            type='text'
+            placeholder='Info@yourgmail.com'
+            className='h-8 pl-3 '
+          />
+          <button className='flex h-full px-2 bg-secondary '>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              stroke-width='1.5'
+              stroke='currentColor'
+              className='w-6 h-8 fill-white stroke-secondary bg-secondary'
+            >
+              <path
+                stroke-linecap='round'
+                stroke-linejoin='round'
+                d='M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5'
+              />
+            </svg>
+          </button>
         </div>
       </div>
     </section>
