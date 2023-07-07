c42d6fbd10aa32dba1a4f7ab5fa4f934636c039d
sohanemon <m.sohanemon@gmail.com>
2023-07-07 14:04:57 +0000

    Jul 7, 2023, 8:04 PM

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from './hero';
+import Working from './working';
 
 export default function Home() {
   return (
     <main className=''>
       <Hero />
+      <Working />
     </main>
   );
 }
---
+++ app/working.tsx
@@ -0,0 +1,9 @@
+export default function Working() {
+  return (
+    <section>
+      <p className='min-w-[282px] text-center text-slate-700 text-[25px] font-bold'>
+        How it works?
+      </p>
+    </section>
+  );
+}
