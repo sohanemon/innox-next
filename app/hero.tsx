9e5eac3713e4562d49ecfabfa69e157fb15f24ae
sohanemon <m.sohanemon@gmail.com>
2023-07-07 13:20:27 +0000

    Jul 7, 2023, 7:20 PM

---
+++ app/hero.tsx
@@ -0,0 +1,31 @@
+export default function Hero() {
+  return (
+    <section className='container pt-24'>
+      <div className='pl-20'>
+        <h1 className='max-w-[344px]  text-[40px] font-bold'>
+          <span className='text-slate-700 '>
+            Invest and grow
+            <br />
+            your{' '}
+          </span>
+          <span className='text-sky-600 '>
+            favourite <br />
+          </span>
+          <span className='text-slate-700 '>creators</span>
+        </h1>
+        <h3 className='max-w-[373px] text-[15px] font-medium'>
+          <span className='text-slate-700 '>
+            Welcome to the first marketplace that allows you to{' '}
+          </span>
+          <span className='text-sky-600 '>invest in content creators</span>
+          <span className='text-slate-700 '>
+            {' '}
+            through tokens
+            <br />
+            and direct funding solutions
+          </span>
+        </h3>
+      </div>
+    </section>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
+import Hero from './hero';
 
 export default function Home() {
   return (
-    <main className='text-4xl mt-20 text-center'>
-      <a href='https://github.com/sohanemon' target='_blank'>sohanemon</a>
+    <main className=''>
+      <Hero />
     </main>
   );
 }
