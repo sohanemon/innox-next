cd64ee0161cfb526fb2765347b0190a0c520c393
sohanemon <m.sohanemon@gmail.com>
2023-07-07 18:23:45 +0000

    Jul 8, 2023, 12:23 AM

--- app/footer.tsx
+++ app/footer.tsx
@@ -1,15 +1,29 @@
 import Brand from '@/components/ui/brand';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Footer() {
   return (
     <section className='flex justify-between flex-wrap gap-10'>
-      <div>
+      <div className='space-y-4'>
         <Brand />
         <p className='text-neutral-800 text-[14px] font-normal leading-snug'>
           Lorem ipsum dolor sit amet,consectetur
           <br />
           adipisicing elit. Quis non,{' '}
         </p>
+        <div className='flex gap-2'>
+          {Array.from(Array(4).keys()).map((_) => (
+            <Link href={'/'} key={_}>
+              <Image
+                src={`/assets/images/${_ + 1}.svg`}
+                alt='   '
+                width={26}
+                height={26}
+              />
+            </Link>
+          ))}
+        </div>
       </div>
     </section>
   );
