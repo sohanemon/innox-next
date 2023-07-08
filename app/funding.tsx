71ed8e345b0103ca393a26af9ba4ac6cc85cef09
sohanemon <m.sohanemon@gmail.com>
2023-07-08 03:50:29 +0000

    Jul 8, 2023, 9:50 AM

--- app/funding.tsx
+++ app/funding.tsx
@@ -5,7 +5,7 @@ import Div2 from './div2';
 
 export default function Funding() {
   return (
-    <section className=''>
+    <section className='max-w-5xl mx-auto'>
       <div className='text-center'>
         <span className='text-slate-700 text-[25px] font-bold'>
           Direct Funding <br />
